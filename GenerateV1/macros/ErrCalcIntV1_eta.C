323f13d487a09d7d228dbab5a3485965e1e0806f
EnrichedWillium-235 <w955m639@ku.edu>
2017-11-09 16:44:56 -0600

    Update

--- GenerateV1/macros/ErrCalcIntV1_eta.C
+++ GenerateV1/macros/ErrCalcIntV1_eta.C
@@ -22,8 +22,6 @@ static const double ptbins[] = {0.30,  0.40,  0.50,  0.60,  0.80,  1.00,  1.25, 
 static const int netabins = 12;
 static const double etabins[] = {-2.4, -2.0, -1.6, -1.2, -0.8, -0.4,  0.0,
                      0.4,  0.8,  1.2,  1.6,  2.0,  2.4};
-static const int nadbins = 6;
-static const double adbins[] = {0.0,  0.4,  0.8,  1.2,  1.6,  2.0,  2.4};
 static const int ncentbins = 8;
 static const int centBins[] = {0, 10, 20, 30, 40, 50, 60, 70, 80};
 static const int nanals = 16;
@@ -51,12 +49,18 @@ TH1D * v123m_eta[nanals][ncentbins];
 TH1D * v123odd_eta[nanals][ncentbins];
 TH1D * v123even_eta[nanals][ncentbins];
 
-TH1D * absDiffv1odd_eta[nanals][ncentbins];
-TH1D * absDiffv1even_eta[nanals][ncentbins];
+TH1D * v1oddStat_eta[nanals][ncentbins];
+TH1D * v1evenStat_eta[nanals][ncentbins];
+
+TH1D * v1oddSyst_eta[nanals][ncentbins];
+TH1D * v1evenSyst_eta[nanals][ncentbins];
+
+TH1D * PerErrv1odd_eta[nanals][ncentbins];
+TH1D * PerErrv1even_eta[nanals][ncentbins];
 
 TH1D * runParms[nanals];
 
-void SystIntV1_eta()
+void ErrCalcIntV1_eta()
 {
 
     TH1::SetDefaultSumw2();
@@ -81,63 +85,35 @@ void SystIntV1_eta()
             v123odd_eta[i][cbin] = (TH1D *) tfin->Get(Form("%s/v1_eta/%d-%d/v123odd_eta_%s_%d",AnalNames[i].data(),centBins[cbin],centBins[cbin+1],AnalNames[i].data(),cbin));
             v123even_eta[i][cbin] = (TH1D *) tfin->Get(Form("%s/v1_eta/%d-%d/v123even_eta_%s_%d",AnalNames[i].data(),centBins[cbin],centBins[cbin+1],AnalNames[i].data(),cbin));
 
-            absDiffv1odd_eta[i][cbin] = new TH1D(Form("absDiffv1odd_eta_%s_%d",AnalNames[i].data(),cbin), "", nadbins, adbins);
-            absDiffv1even_eta[i][cbin] = new TH1D(Form("absDiffv1even_eta_%s_%d",AnalNames[i].data(),cbin), "", nadbins, adbins);
+            PerErrv1odd_eta[i][cbin] = new TH1D(Form("PerErrv1odd_eta_%s_%d",AnalNames[i].data(),cbin), "", netabins, etabins);
+            PerErrv1even_eta[i][cbin] = new TH1D(Form("PerErrv1even_eta_%s_%d",AnalNames[i].data(),cbin), "", netabins, etabins);
+
+            v1oddStat_eta[i][cbin] = new TH1D(Form("v1oddStat_eta_%s_%d",AnalNames[i].data(),cbin), "", netabins, etabins);
+            v1evenStat_eta[i][cbin] = new TH1D(Form("v1evenStat_eta_%s_%d",AnalNames[i].data(),cbin), "", netabins, etabins);
+
+            v1oddSyst_eta[i][cbin] = new TH1D(Form("v1oddSyst_eta_%s_%d",AnalNames[i].data(),cbin), "", netabins, etabins);
+            v1evenSyst_eta[i][cbin] = new TH1D(Form("v1evenSyst_eta_%s_%d",AnalNames[i].data(),cbin), "", netabins, etabins);
         }
     }
 
-    // calculate absolute differences
+    // symmetrize v1^odd and v1^even
     for (int i = 0; i<nanals; i++) {
         for (int cbin = 0; cbin<ncentbins; cbin++) {
-            for (int j = 0; j<6; j++) {
-                double v1odd_pos = v1odd_eta[i][cbin]->GetBinContent(12-j);
+            for (int j = 0; j<netabins; j++) {
+                double v1odd_pos = v1odd_eta[i][cbin]->GetBinContent(netabins-j);
                 double v1odd_neg = v1odd_eta[i][cbin]->GetBinContent(j+1);
-                double v1odd_pos_err = v1odd_eta[i][cbin]->GetBinError(12-j);
+                double v1odd_pos_err = v1odd_eta[i][cbin]->GetBinError(netabins-j);
                 double v1odd_neg_err = v1odd_eta[i][cbin]->GetBinError(j+1);
 
-                double adodd = fabs(v1odd_pos) - fabs(v1odd_neg);
-                double adodd_err = sqrt( pow(v1odd_pos_err,2) + pow(v1odd_neg_err,2) );
-                absDiffv1odd_eta[i][cbin]->SetBinContent(j+1, adodd);
-                absDiffv1odd_eta[i][cbin]->SetBinError(j+1, adodd_err);
-
-                double v1even_pos = v1even_eta[i][cbin]->GetBinContent(12-j);
-                double v1even_neg = v1even_eta[i][cbin]->GetBinContent(j+1);
-                double v1even_pos_err = v1even_eta[i][cbin]->GetBinError(12-j);
-                double v1even_neg_err = v1even_eta[i][cbin]->GetBinError(j+1);
-
-                double adeven = fabs(v1even_pos) - fabs(v1even_neg);
-                double adeven_err = sqrt( pow(v1even_pos_err,2) + pow(v1even_neg_err,2) );
-                absDiffv1even_eta[i][cbin]->SetBinContent(j+1, adeven);
-                absDiffv1even_eta[i][cbin]->SetBinError(j+1, adeven_err);
+                double v1odd = 0.5*(v1odd_pos + v1odd_neg);
+                // if (j<6 || (v1odd_pos < 0 && v1odd_neg < 0)) v1odd*=-1;
+                double v1odd_stat_err = 0.5*sqrt( v1odd_pos_err*v1odd_pos_err + v1odd_neg_err*v1odd_neg_err );
+                //if (cbin == 0 && i == 7) cout<<v1odd<<"\t"<<v1odd_stat_err<<endl;
+                // v1oddStat_eta[i][cbin]->SetBinContent(netabins-j, v1odd);
+                // v1oddStat_eta[i][cbin]->SetBinError(netabins-j, v1odd_stat_err);
             }
         }
     }
-
-    //-- plotting options
-
-    for (int i = 0; i<nanals; i++) {
-        for (int cbin = 0; cbin<ncentbins; cbin++) {
-            absDiffv1odd_eta[i][cbin]->SetMarkerColor(kBlack);
-            absDiffv1odd_eta[i][cbin]->SetLineColor(kBlack);
-            absDiffv1odd_eta[i][cbin]->SetMarkerStyle(21);
-            absDiffv1odd_eta[i][cbin]->SetMarkerSize(1.1);
-
-            absDiffv1even_eta[i][cbin]->SetMarkerColor(kBlack);
-            absDiffv1even_eta[i][cbin]->SetLineColor(kBlack);
-            absDiffv1even_eta[i][cbin]->SetMarkerStyle(21);
-            absDiffv1even_eta[i][cbin]->SetMarkerSize(1.1);
-        }
-    }
-
-
-    //-- make plots
-    if (!fopen("plots","r")) system("mkdir plots");
-    if (!fopen("plots/intv1","r")) system("mkdir plots/intv1");
-    if (!fopen("plots/intv1/intv1_eta","r")) system("mkdir plots/intv1/intv1_eta");
-
-    int anal; // choice of analysis
-
-
 
 
 }
